17ac27a0847087204971c0df7d5da95be77429ff
lookuptrendzofficial <lookuptrendzofficial@gmail.com>
2026-07-25 06:09:55 +0530

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Link from "next/link";
 const projects = [
   {
